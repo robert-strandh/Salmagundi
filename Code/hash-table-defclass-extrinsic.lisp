51430d713f613c2d88c048d86cdf30839f6d7743
Robert Strandh <robert.strandh@gmail.com>
2024-07-28 09:13:43 +0200

    New component with extrinsic definition of class HASH-TABLE.

---
+++ Code/hash-table-defclass-extrinsic.lisp
@@ -0,0 +1,12 @@
+(cl:in-package #:salmagundi)
+
+(defclass hash-table ()
+  ((%test :initform #'eql
+          :accessor %%hash-table-test
+          :reader %hash-table-test)
+   (%rehash-size :initarg :rehash-size
+                 :initform 1.5
+                 :reader hash-table-rehash-size)
+   (%rehash-threshold :initarg :rehash-threshold
+                      :initform 0.8
+                      :reader hash-table-rehash-threshold)))
